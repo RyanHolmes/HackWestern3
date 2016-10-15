e31457af5b60805af36b82fd62fe886af6ff3a6a
Ryan Holmes <ryankaz94@gmail.com>
2016-10-14 23:14:05 -0400

    more setup

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 $(document).ready(function(){
   $("#poop").click(function(){
+    // TODO needs to be array of jquery obects with a counter
     $("#slide1").addClass("hidden");
     $("#slide2").removeClass("hidden");
-    $("#slide2").addClass("slideUp");
+    $("#slide2").addClass("expandUp");
+  });
+
+  $("#menu-toggle").click(function(){
+    $("#side-nav").removeClass("hidden");
+    $("#side-nav").addClass("stretchRight");
   });
 });
