1b5407f598b19c585d8a9c8e8a0250eec45bc131
Ryan Holmes <ryankaz94@gmail.com>
2016-10-14 22:20:55 -0400

    basic animation stuff

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  $("#poop").click(function(){
+    $("#slide1").addClass("hidden");
+    $("#slide2").removeClass("hidden");
+    $("#slide2").addClass("slideUp");
+  });
+});
